6719f01f0f887ebe93b857bbee4c76d4bcd5ab9a
tahame01 <tahame01@dcs.bbk.ac.uk>
2020-11-04 19:12:58 +0000

    menubar

--- menubar.py
+++ menubar.py
@@ -6,18 +6,42 @@ from tkinter import Menu
 root = tk.Tk()
 
 
+try:
+    from ctypes import windll
+    windll.shcore.SetProcessDpiAwareness(1)
+except:
+    pass
+
+def _quit():
+    root.quit()
+    root.destroy()
+    exit()
+
+
 #Creating menubar
 menu_bar = Menu(root)
 root.config(menu=menu_bar)
 
-#Creating objects in menubar
-file_menu = Menu(menu_bar)
+#Creating file menu objects in menubar
+file_menu = Menu(menu_bar,tearoff=0) #tearoff will tear off the line
 file_menu.add_command(label='New')
 file_menu.add_command(label='Home')
-file_menu.add_separator()
+file_menu.add_separator()  #adding seperator
 file_menu.add_command(label='Datasets')
 
+##creating second data menu objects
+data_menu = Menu(menu_bar,tearoff=0)
+data_menu.add_command(label='Datasets')
+data_menu.add_command(label='Graph')
+
+#Creating exit menu
+exit_menu = Menu(menu_bar,tearoff=0)
+exit_menu.add_command(label='quit',command=_quit)
+
 menu_bar.add_cascade(label='File',menu=file_menu)
+menu_bar.add_cascade(label='Data',menu=data_menu)
+menu_bar.add_cascade(label='Exit',menu=exit_menu)
+
 
 
 
