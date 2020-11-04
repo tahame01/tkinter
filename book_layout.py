1e54255db2519303b93132d1cb374edcc7ee056b
tahame01 <tahame01@dcs.bbk.ac.uk>
2020-11-04 13:43:06 +0000

    important functions been added

--- book_layout.py
+++ book_layout.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 root = tk.Tk()
 
 button_frame = ttk.LabelFrame(root,text='this is label frame')
-button_frame.grid(column=0,row=7)
+button_frame.grid(column=0,row=7,padx=40,pady=40)
 
 
 # label1 = ttk.Label(button_frame,text='label1').grid(row=0,column=0)
@@ -17,7 +17,8 @@ label2 = ttk.Label(button_frame,text='label2').grid(row=1,column=0)
 label3 = ttk.Label(button_frame,text='label3').grid(row=2,column=0)
 
 
-
+for child in button_frame.winfo_children():
+    child.grid_configure(padx=20,pady=20)
 
 
 
