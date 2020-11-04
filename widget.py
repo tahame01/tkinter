da047ef0604f1f9b367e96ef2cb50d40773c437b
tahame01 <tahame01@dcs.bbk.ac.uk>
2020-11-04 16:37:34 +0000

    Widget and image

---
+++ widget.py
@@ -0,0 +1,25 @@
+import tkinter as tk 
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+try:
+    from ctypes import windll
+    windll.shcore.SetProcessDpiAwareness(1)
+except:
+    pass
+
+
+root = tk.Tk()
+
+image = Image.open('hack.jpg').resize((500,500))
+photo = ImageTk.PhotoImage(image)
+
+label = ttk.Label(root,image=photo,padding=10)
+label.pack()
+
+button =ttk.Button(root,text='submit').pack()
+
+
+
+
+root.mainloop()
