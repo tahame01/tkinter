dc30e4064b3e5f19521f3da3d97ca632148f4e04
tahame01 <tahame01@dcs.bbk.ac.uk>
2020-11-04 11:39:11 +0000

    combo box added

--- book_chap-1.py
+++ book_chap-1.py
@@ -20,13 +20,19 @@ label1.grid(row=0,column=0)
 
 text_entry = ttk.Entry(win,width=12,textvariable=name)
 text_entry.grid(row=1,column=0)
+text_entry.focus()
 
 #ttk button don't have background property
 button1 = ttk.Button(win,text='submit',command=clicked)
 button1.grid(row=2,column=0)
 
+combo_box = ttk.Combobox(text='select a number:',values=(1,2,3,4))
+combo_box.grid(row=3,column=0)
+combo_box.current=0
+
+
 button2 = ttk.Button(win,text='close',command=win.destroy)
-button2.grid(row=2,column=1)
+button2.grid(row=4,column=0)
 
 
 
