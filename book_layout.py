c80ee1b3b461f9e247925bc49a996068af9847fb
tahame01 <tahame01@dcs.bbk.ac.uk>
2020-11-04 13:54:59 +0000

    important functions been added

--- book_layout.py
+++ book_layout.py
@@ -16,7 +16,13 @@ label1 = ttk.Label(button_frame,text='label1').grid(row=0,column=0)
 label2 = ttk.Label(button_frame,text='label2').grid(row=1,column=0)
 label3 = ttk.Label(button_frame,text='label3').grid(row=2,column=0)
 
+#The grid_configure() function enables us to modify the UI elements before the main
+# The winfo_children() function returns a list of all the children belonging to the
+# buttons_frame variable. This enables us to loop through them and assign the padding to
+# each label.
 
+
+loop displays them. 
 for child in button_frame.winfo_children():
     child.grid_configure(padx=20,pady=20)
 
