1022fbb6cb1e11de0904bdd6f32fb46e026cd603
tahame01 <tahame01@dcs.bbk.ac.uk>
2020-11-04 11:28:39 +0000

    added

--- book_chap-1.py
+++ book_chap-1.py
@@ -6,23 +6,26 @@ from tkinter import ttk
 win = tk.Tk()
 win.title('test window')
 
+name = tk.StringVar()
+
 
 def clicked():
-    label1.configure(text='i am clicked',background='green')
-    button1.configure(text='yellow')
+    label1.configure(text='Hello '+ name.get(),background='yellow')
+    button1.configure(state='disabled')
 
 
-
-label1 = ttk.Label(win,text='i will change')
+label1 = ttk.Label(win,text='Please input your name please ')
 label1.grid(row=0,column=0)
 
+text_entry = ttk.Entry(win,width=12,textvariable=name)
+text_entry.grid(row=1,column=0)
 
 #ttk button don't have background property
 button1 = ttk.Button(win,text='submit',command=clicked)
-button1.grid(row=1,column=0)
+button1.grid(row=2,column=0)
 
 button2 = ttk.Button(win,text='close',command=win.destroy)
-button2.grid(row=1,column=1)
+button2.grid(row=2,column=1)
 
 
 
