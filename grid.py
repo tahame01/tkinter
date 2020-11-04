43da1f8c7a3d4185dac734d68d239386ee6ae0c1
tahame01 <tahame01@dcs.bbk.ac.uk>
2020-11-04 10:32:17 +0000

    added

--- grid.py
+++ grid.py
@@ -3,32 +3,45 @@ from tkinter import ttk
 
 
 root = tk.Tk()
+input = tk.StringVar()
+
+
+def button1():
+    print(f" hello {input.get() or 'not given any word'}")
+
+
+root.columnconfigure(0, weight=1)
+#root.rowconfigure(1, weight=1)
 
 
 #check out ttk frame padding property!!
-frame1 = tk.Frame(root,bg='yellow')
-frame1.grid(row=0,column=7)
+frame1 = ttk.Frame(root,padding=(20,10,20,10))
+frame1.grid(sticky='EW')
 
-#frame1.columnconfigure(0,weight=1)
-frame2 = tk.Frame(root,bg='green')
-frame2.grid(row=1,column=4)
 
-label1 = tk.Label(frame1,text='Label1',bg='blue')
-label1.grid(row=0,column=4)
+frame2 = ttk.Frame(root,padding=(20,10,20,10))
+frame2.grid(sticky='EW')
 
-# text_entry = ttk.Entry(frame1,width=20,textvariable=input)
-# text_entry.grid(row=1,column=2)
-# text_entry.focus()
+frame2.columnconfigure(0,weight=1)
+
+
+
+label1 = ttk.Label(frame1,text='Name: ')
+label1.grid(row=0,column=0)
+label1.configure(background='red')
+
+text_entry = ttk.Entry(frame1,width=15,textvariable=input)
+text_entry.grid(row=0,column=1)
+text_entry.focus()
 # text_entry.columnconfigure(0,weight=1)
 
-# button1 = tk.Button(frame2,text='click me',bg='green',fg='black')
-# button1.grid(row=1,column=1)
+button1 = ttk.Button(frame1,text='submit',command=button1)
+button1.grid(row=1,column=0)
 
 
 
-# button2 = tk.Button(frame2,bg='red',text='click me')
-# button2.grid(row=1,column=2)
-
+button2 = ttk.Button(frame1,text='close',command=root.destroy)
+button2.grid(row=1,column=1)
 
 
 
