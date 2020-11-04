b300a40cd22394ff041169082a16a335dfc1195c
tahame01 <tahame01@dcs.bbk.ac.uk>
2020-11-04 16:43:04 +0000

    Widget and image

--- widget.py
+++ widget.py
@@ -14,7 +14,7 @@ root = tk.Tk()
 image = Image.open('hack.jpg').resize((500,500))
 photo = ImageTk.PhotoImage(image)
 
-label = ttk.Label(root,image=photo,padding=10)
+label = ttk.Label(root,image=photo,padding=10,text='Hacker buddy!!',compound='right')
 label.pack()
 
 button =ttk.Button(root,text='submit').pack()
