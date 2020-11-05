a7977f56e7fb78d9328a16e7225887d11d9b9414
tahame01 <tahame01@dcs.bbk.ac.uk>
2020-11-05 10:35:31 +0000

    datatable

--- pandasTable.py
+++ pandasTable.py
@@ -3,18 +3,22 @@ from pandastable import Table
 from tkinter import * 
 # Load CSV using Pandas
 from pandas import read_csv
+import datatable as dt
 
-filename = 'pima-indians-diabetes.data.csv'
-names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
-data = read_csv(filename, names=names)
-data1=data.head()
+
+# filename = 'pima-indians-diabetes.data.csv'
+# names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+# data = read_csv(filename, names=names)
+# data1=data.head()
+df = dt.fread('pima-indians-diabetes.data.csv')
+pandas_df = df.to_pandas()
 
 root=Tk()
 frame = Frame(root)
 frame.pack()
 
-table=Table(frame,dataframe=data1,showtoolbar=True,showstatusbar=True)
-
+# table=Table(frame,dataframe=data1,showtoolbar=True,showstatusbar=True)
+table=Table(frame,dataframe=pandas_df,showtoolbar=True,showstatusbar=True)
 
 table.show()
 
