59a2e23fa3a461f3b5ea88acf46d27c697ef1663
tahame01 <tahame01@dcs.bbk.ac.uk>
2020-11-05 11:32:29 +0000

    pandas table

--- pandasTable.py
+++ pandasTable.py
@@ -6,6 +6,13 @@ from pandas import read_csv
 import datatable as dt
 
 
+try:
+    from ctypes import windll
+    windll.shcore.SetProcessDpiAwareness(1)
+except:
+    pass
+
+
 # filename = 'pima-indians-diabetes.data.csv'
 # names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
 # data = read_csv(filename, names=names)
@@ -15,11 +22,29 @@ pandas_df = df.to_pandas()
 
 root=Tk()
 frame = Frame(root)
-frame.pack()
+
 
 # table=Table(frame,dataframe=data1,showtoolbar=True,showstatusbar=True)
 table=Table(frame,dataframe=pandas_df,showtoolbar=True,showstatusbar=True)
-
+table.cellbackgr ='powderblue'
+table.grid_color ='red'
 table.show()
 
+frame.pack()
+
+
 root.mainloop()
+
+# {'align': 'w',
+#  'cellbackgr': '#F4F4F3',
+#  'cellwidth': 80,
+#  'colheadercolor': '#535b71',
+#  'floatprecision': 2,
+#  'font': 'Arial',
+#  'fontsize': 12,
+#  'fontstyle': '',
+#  'grid_color': '#ABB1AD',
+#  'linewidth': 1,
+#  'rowheight': 22,
+#  'rowselectedcolor': '#E4DED4',
+#  'textcolor': 'black'}
